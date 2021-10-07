6d57b04fc477db01c0b15d47a0539db6153d87f4
tekdoken <tekdokenzx@gmail.com>
2021-10-07 10:26:53 +0700

    Initial commit

---
+++ src/Bai1/SystemTime.java
@@ -0,0 +1,10 @@
+package Bai1;
+
+import java.util.Date;
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
---
+++ src/Bai1/Toantu.java
@@ -0,0 +1,9 @@
+package Bai1;
+
+public class Toantu {
+        public static void main(String[] args) {
+            float width;
+            float height;
+        }
+    }
+
