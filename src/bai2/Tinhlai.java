7a6d5892364acbf16fba0daaf4b0b84871b07ad2
tekdoken <tekdokenzx@gmail.com>
2021-10-08 09:54:21 +0700

    bai2

---
+++ src/bai2/Tinhlai.java
@@ -0,0 +1,20 @@
+package bai2;
+
+import java.util.Scanner;
+
+public class Tinhlai {
+    public static void main(String[] args) {
+        Scanner sa = new Scanner(System.in);
+        System.out.print("nhap so tien gui: ");
+        double tien = sa.nextDouble();
+        System.out.print("phan tram lai: ");
+        double ptl = sa.nextDouble();
+        System.out.print("nhap so thang: ");
+        int thang = sa.nextInt();
+        double lai = 0;
+        for (int i = 0; i < thang; i++) {
+            lai += tien * (ptl / 100) / 12;
+        }
+        System.out.println("tong tien lai la: "+lai);
+    }
+}
