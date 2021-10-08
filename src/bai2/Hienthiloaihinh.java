8a360c923e44421848ad714380ea6469203b5a72
tekdoken <tekdokenzx@gmail.com>
2021-10-08 10:36:05 +0700

    bai2

---
+++ src/bai2/Hienthiloaihinh.java
@@ -0,0 +1,7 @@
+package bai2;
+
+public class Hienthiloaihinh {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/bai2/Vemenu.java
@@ -0,0 +1,46 @@
+package bai2;
+
+import java.util.Scanner;
+
+public class Vemenu {
+    public static void main(String[] args) {
+        Scanner sa = new Scanner(System.in);
+        int chon = sa.nextInt();
+        System.out.println("Menu");
+        System.out.println("1. Draw the triangle");
+        System.out.println("2. Draw the square");
+        System.out.println("3. Draw the rectangle");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice: ");
+        switch (chon) {
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("No choice!");
+        }
+    }
+}
