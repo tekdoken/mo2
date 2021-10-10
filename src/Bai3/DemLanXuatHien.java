e3c99c4d5eed4f7675513998eecdc460e3e5237e
tekdoken <tekdokenzx@gmail.com>
2021-10-11 01:32:38 +0700

    bai3

---
+++ src/Bai3/DemLanXuatHien.java
@@ -0,0 +1,20 @@
+package Bai3;
+
+import java.util.Scanner;
+
+public class DemLanXuatHien {
+    public static void main(String[] args) {
+        Scanner sa= new Scanner(System.in);
+        String s="manafa";
+        String cs=sa.nextLine();
+        char[] ch =cs.toCharArray();
+        char[] str=s.toCharArray();
+        int c=0;
+        for(int i=0;i<str.length;i++){
+            if(ch[0]==str[i]){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
