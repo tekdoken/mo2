e71b5163c469e54ae778325ec5704dbd1dffbb30
tekdoken <tekdokenzx@gmail.com>
2021-10-28 15:53:29 +0700

    bai15

---
+++ src/bai16/bt1/ReaderWriter.java
@@ -0,0 +1,28 @@
+package bai16.bt1;
+
+//import bai16.th1.ReadFileExample;
+
+import java.io.*;
+
+public class ReaderWriter {
+    public static void main(String[] args) {
+        try{
+            BufferedWriter wr = new BufferedWriter(new FileWriter("src\\bai16\\th2\\result1.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("D:\\num.txt"));
+            File file = new File("D:\\num.txt");
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            String i = "";
+            while ((i = br.readLine()) != null) {
+                wr.write("\n" + i);
+            }
+            br.close();
+            wr.close();
+        }catch (Exception e){
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+    }
+
+}
