82e1af924961ea00bca908e4299ae4442ae3836c
tekdoken <tekdokenzx@gmail.com>
2021-10-29 08:12:21 +0700

    bai15

--- src/miniTest2710/display/DisplayMenuStudent.java
+++ src/miniTest2710/display/DisplayMenuStudent.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 
 public class DisplayMenuStudent {
     public static int idd = 1;
-
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     public static void main(String[] args) throws IOException {
 
         ManageStudent manageStudent = new ManageStudent();
@@ -105,7 +113,7 @@ public class DisplayMenuStudent {
                                 bw.write(str);
                                 bw.close();
                             } catch (FileNotFoundException e) {
-                                System.err.println("The system cannot find the file specified");
+                                System.out.println(ANSI_RED + "The system cannot find the file specified"+ ANSI_RESET);
                             } finally {
                                 break;
                             }
@@ -133,7 +141,7 @@ public class DisplayMenuStudent {
                                 }
                                 manageStudent.print();
                             } catch (FileNotFoundException e) {
-                                System.err.println("The system cannot find the file specified");
+                                System.out.println(ANSI_RED + "The system cannot find the file specified"+ ANSI_RESET);
                             } finally {
                                 break;
                             }
