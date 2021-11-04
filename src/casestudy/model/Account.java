0a026ccb0cc527837a50f5ef57df1a3bd957adf0
tekdoken <tekdokenzx@gmail.com>
2021-11-04 17:13:59 +0700

    bai17

--- src/casestudy/model/Account.java
+++ src/casestudy/model/Account.java
@@ -4,11 +4,56 @@ public class Account {
     private String userName;
     private int password;
     private int id;
-    private String nameAlbum;
-
-
+private Album album;
     public Account() {
     }
 
+    public Account(String userName, int password, int id, Album album) {
+        this.userName = userName;
+        this.password = password;
+        this.id = id;
+        this.album = album;
+    }
 
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "userName='" + userName + '\'' +
+                ", password=" + password +
+                ", id=" + id +
+                ", album=" + album +
+                '}';
+    }
 }
--- src/casestudy/model/Album.java
+++ src/casestudy/model/Album.java
@@ -2,9 +2,48 @@ package casestudy.model;
 
 public class Album {
     private String nameAlbum;
+    private int id;
+    private Song song;
 
     public Album() {
     }
 
+    public Album(String nameAlbum, int id, Song song) {
+        this.nameAlbum = nameAlbum;
+        this.id = id;
+        this.song = song;
+    }
 
+    public String getNameAlbum() {
+        return nameAlbum;
+    }
+
+    public void setNameAlbum(String nameAlbum) {
+        this.nameAlbum = nameAlbum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "nameAlbum='" + nameAlbum + '\'' +
+                ", id=" + id +
+                ", song=" + song +
+                '}';
+    }
 }
