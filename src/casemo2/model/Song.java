45b2c0c516a660eec0132a7d68e22340d3e32aa3
tekdoken <tekdokenzx@gmail.com>
2021-11-08 13:54:19 +0700

    2

--- src/casemo2/model/Song.java
+++ src/casemo2/model/Song.java
@@ -7,27 +7,8 @@ import java.util.List;
 
 public class Song implements  Serializable {
     private String name;
-    private List<Song> listSong;
 
-    public List<Song> getSongs() {
-        return listSong;
-    }
 
-    public void setSongs(Song songs) {
-        this.listSong.add(songs);
-    }
-
-    public Song() {
-    }
-
-    public Song(List<Song> listSong) {
-        this.listSong = listSong;
-    }
-
-    public Song(String name, List<Song> listSong) {
-        this.name = name;
-        this.listSong = listSong;
-    }
 
     public Song(String name) {
         this.name = name;
